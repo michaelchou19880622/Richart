b5db1dac4e701cefb0f389aeb3f1f6d181a51418
michaelchou-xtremeapp <mic_chou@hpicorp.com.tw>
2020-03-26 10:03:44 +0800

    [ 優化 ] 增加debug trace log

--- com.bcs.core.richart/src/main/java/com/bcs/core/richart/service/RichartAdService.java
+++ com.bcs.core.richart/src/main/java/com/bcs/core/richart/service/RichartAdService.java
@@ -16,68 +16,81 @@ import com.bcs.core.enums.CONFIG_STR;
 import com.bcs.core.resource.CoreConfigReader;
 import com.bcs.core.richart.api.model.AdUserSyncModel;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class RichartAdService {
 	@Autowired
 	private AdminUserService adminUserService;
 	@Autowired
-    private PasswordEncoder passwordEncoder;
-	
-	/** Logger */
-	private static Logger logger = Logger.getLogger(RichartAdService.class);
-	
-	@Transactional(rollbackFor=Exception.class, timeout = 30)
-	public void syncAdUser(AdUserSyncModel model) throws Exception{
-		logger.info("syncAdUser");
-		
+	private PasswordEncoder passwordEncoder;
+
+	@Transactional(rollbackFor = Exception.class, timeout = 30)
+	public void syncAdUser(AdUserSyncModel model) throws Exception {
+		log.info("[syncAdUser] AdUserSyncModel = {}", model);
+
 		String account = model.getUserId();
-		if(StringUtils.isBlank(account)){
+		
+		if (StringUtils.isBlank(account)) {
 			throw new Exception("AccountNull");
 		}
-		
+
 		AdminUser adminUser = adminUserService.findOne(account);
-		
-		if(adminUser == null){
+		log.info("[syncAdUser] 1-1 AdminUser = {}", adminUser);
+
+		if (adminUser == null) {
 			adminUser = new AdminUser();
 			adminUser.setAccount(account);
 			adminUser.setModifyTime(new Date());
 		}
+
+		log.info("[syncAdUser] 1-2 AdminUser = {}", adminUser);
+		
 		String prefix = CoreConfigReader.getString(CONFIG_STR.PASSWORD_PREFIX, true);
 		String suffix = CoreConfigReader.getString(CONFIG_STR.PASSWORD_SUFFIX, true);
+		
 		account = account.toLowerCase();
 		adminUser.setPassword(passwordEncoder.encode(prefix + account + suffix));
-		
+
 		adminUser.setModifyUser("SYSTEM");
 
 		String name = model.getName();
-		if(StringUtils.isBlank(name)){
+		
+		if (StringUtils.isBlank(name)) {
 			throw new Exception("NameNull");
 		}
+		
 		adminUser.setUserName(name);
 
 		String linebc = model.getLinebc();
 		String role = "";
+		
 		boolean roleCheck = false;
-		if(StringUtils.isBlank(linebc)){
+		
+		if (StringUtils.isBlank(linebc)) {
 			throw new Exception("LinebcNull");
 		}
+		
 		RoleCode[] roles = RoleCode.values();
-		for(RoleCode code : roles){
-			if(code.getRoleName().equals(linebc)){
+		
+		for (RoleCode code : roles) {
+			if (code.getRoleName().equals(linebc)) {
 				role = code.getRoleId();
 				roleCheck = true;
 			}
 		}
-		for(RoleCode code : roles){
-			if(code.getRoleNameEn().equals(linebc)){
+		
+		for (RoleCode code : roles) {
+			if (code.getRoleNameEn().equals(linebc)) {
 				role = code.getRoleId();
 				roleCheck = true;
 			}
 		}
-		if(roleCheck){
+		
+		if (roleCheck) {
 			adminUser.setRole(role);
-		}
-		else{
+		} else {
 			throw new Exception("RoleError");
 		}
 
@@ -88,13 +101,13 @@ public class RichartAdService {
 		adminUser.setRobot(model.getRobot());
 		adminUser.setDesk(model.getDesk());
 		adminUser.setLinebc(model.getLinebc());
-        adminUser.setRoseline(model.getRoseline());
-		
+		adminUser.setRoseline(model.getRoseline());
+
 		adminUser.setViewLimit(model.getViewLimit());
 		adminUser.setCanCopy(model.getCanCopy());
 		adminUser.setCanSave(model.getCanSave());
 		adminUser.setCanPrinting(model.getCanPrinting());
-		
+
 		adminUserService.save(adminUser);
 	}
 }
