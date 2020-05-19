d26ef89e9ff4baaa4cbeefc4119dc4b44f272bfe
michaelchou0622-CPT <mic_chou@hpicorp.com.tw>
2020-05-19 12:57:25 +0800

    1. 新增api接口 - /api/replyMessageWithReplyToken 供碩網關鍵字回call使用 2. BotReplyRecordService 新增 findByReplyToken 依照replyToken檢查使用

--- com.bcs.core.richart/src/main/java/com/bcs/core/richart/api/controller/ConversationalCommerceController.java
+++ com.bcs.core.richart/src/main/java/com/bcs/core/richart/api/controller/ConversationalCommerceController.java
@@ -5,6 +5,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +23,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpClientErrorException;
 
 import com.bcs.core.api.service.LineProfileService;
+import com.bcs.core.bot.db.entity.BotReplyRecord;
+import com.bcs.core.bot.db.service.BotReplyRecordService;
 import com.bcs.core.enums.CONFIG_STR;
 import com.bcs.core.enums.LINE_HEADER;
 import com.bcs.core.resource.CoreConfigReader;
 import com.bcs.core.smartrobot.service.SwitchIconService;
+import com.bcs.core.spring.ApplicationContextProvider;
 import com.bcs.core.utils.CryptUtil;
+import com.bcs.core.utils.DataUtils;
 import com.bcs.core.utils.RestfulUtil;
 
 import lombok.extern.slf4j.Slf4j;
@@ -136,6 +141,108 @@ public class ConversationalCommerceController {
 			}
 		}
 	}
+
+    @RequestMapping(method = RequestMethod.POST, value = "/replyMessageWithReplyToken", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<?> replyMessageWithReplyToken(HttpServletRequest request, HttpServletResponse response, 
+								    		@RequestBody String requestBodyString) {
+    	log.info("[replyMessageWithReplyToken] Requestbody: " + DataUtils.toPrettyJsonUseJackson(requestBodyString));
+
+        try {
+            if (request.getHeader(HttpHeaders.AUTHORIZATION) == null) {
+                return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"missing headers\"}", HttpStatus.BAD_REQUEST);
+            }
+
+            String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+            if (authorization.split("Basic ").length != 2) {
+                return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"invalid authorization format\"}", HttpStatus.UNAUTHORIZED);
+            }
+
+            String token = authorization.split("Basic ")[1];
+            log.info("token = {}", token);
+            
+            String secret = CoreConfigReader.getString(CONFIG_STR.AES_SECRET_KEY, true);
+            log.info("secret = {}", secret);
+            
+            String iv = CoreConfigReader.getString(CONFIG_STR.AES_INITIALIZATION_VECTOR, true);
+            log.info("iv = {}", iv);
+            
+            String originalToken = CoreConfigReader.getString(CONFIG_STR.API_ORIGINAL_TOKEN, true);
+            log.info("originalToken = {}", originalToken);
+            
+            String decrytedString = CryptUtil.Decrypt(CryptUtil.AES, token, secret, iv);
+            log.info("decrytedString = {}", decrytedString);
+
+            if (!decrytedString.equals(originalToken)) {
+                return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"invalid token\"}", HttpStatus.UNAUTHORIZED);
+            }
+
+            String accessToken = CoreConfigReader.getString(CONFIG_STR.Default.toString(), CONFIG_STR.ChannelToken.toString(), true);
+            String serviceCode = CoreConfigReader.getString(CONFIG_STR.AutoReply.toString(), CONFIG_STR.ChannelServiceCode.toString(), true);
+            
+            /* 設定 request headers */
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+            headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            headers.set(LINE_HEADER.HEADER_BOT_ServiceCode.toString(), serviceCode);
+    		log.info("headers = {}", headers);
+            
+            JSONObject requestBody = new JSONObject(requestBodyString);
+            log.info("requestBody = {}", requestBody.toString());
+
+            switchIconService.appendSender(CONFIG_STR.AutoReply.toString(), requestBody);
+            log.info("requestBody after append Sender = {}", requestBody.toString());
+            
+            /* 解析 replyToken */
+            String replyToken = requestBody.getString("replyToken");
+            log.info("replyToken = {}", replyToken);
+
+            /* 檢查 BOT_REPLY_RECORD 是否有此 replyToken 對應的紀錄? */
+            BotReplyRecordService botReplyRecordService = ApplicationContextProvider.getApplicationContext().getBean(BotReplyRecordService.class);
+            BotReplyRecord botReplyRecord = botReplyRecordService.findByReplyToken(replyToken);
+            log.info("botReplyRecord = {}", botReplyRecord);
+            
+            if (StringUtils.isBlank(replyToken)) {
+            	return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"invalid reply token\"}", HttpStatus.UNAUTHORIZED);
+            } else if (botReplyRecord == null) {
+            	return new ResponseEntity<>(String.format("{\"error\": \"true\", \"message\": \"can not find the bot reply record with this reply token : %s\"}", replyToken), HttpStatus.UNAUTHORIZED);
+			}
+            
+            String url = CoreConfigReader.getString(CONFIG_STR.LINE_MESSAGE_REPLY_URL.toString());
+
+            /* 將 headers 跟 body 塞進 HttpEntity 中 */
+            HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody.toString(), headers);
+
+            RestfulUtil restfulUtil = new RestfulUtil(HttpMethod.POST, url, httpEntity);
+
+            JSONObject jsonObjectResult = restfulUtil.execute();
+            log.info("jsonObjectResult = {}", jsonObjectResult);
+
+            return new ResponseEntity<>("{\"error\": \"false\", \"message\": \"success\"}", HttpStatus.OK);
+        } catch (Exception e) {
+            if (e instanceof BadPaddingException || e instanceof IllegalBlockSizeException || e instanceof IllegalArgumentException) {
+                return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"invalid token\"}", HttpStatus.UNAUTHORIZED);
+            } else if (e instanceof HttpClientErrorException) {
+                String responseMessage = ((HttpClientErrorException) e).getResponseBodyAsString();
+                HttpStatus responseStatusCode = ((HttpClientErrorException) e).getStatusCode();
+
+                if (responseMessage.contains("{\"message\"")) {
+                    JSONObject responseMessageObject = new JSONObject(responseMessage);
+                    String message = responseMessageObject.getString("message");
+
+                    return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"" + message + "\"}", HttpStatus.BAD_REQUEST);
+                } else {
+                    return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"" + e.getMessage() + "\"}", responseStatusCode);
+                }
+            } else if (e instanceof JSONException) {
+                return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"The request body has 1 error(s)\"}", HttpStatus.BAD_REQUEST);
+            } else {
+                String errorMsg = e.getMessage();
+
+                return new ResponseEntity<>("{\"error\": \"true\", \"message\": \"" + errorMsg + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/getNickname", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<?> getNickname(HttpServletRequest request, HttpServletResponse response, @RequestParam String UID) {
