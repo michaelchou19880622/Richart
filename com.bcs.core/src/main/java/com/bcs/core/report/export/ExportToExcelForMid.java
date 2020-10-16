b12177dc852fed451db2e9f3afcbb95d77c467cf
hongyi <hikuo123@gmail.com>
2020-10-16 17:18:07 +0800

    <優化> 群組匯出功能支援超過104萬筆以上(1048576)的資料. 1. 每個sheet超過百萬筆資料時, 產生新的sheet. 預計作法可以最多支援匯出2500萬筆資料,不過實際上在SIT測試大概頂多只能匯出400多萬筆, 500萬筆以上就有機會碰到記憶體不足的問題(Out Of Memory.)

--- com.bcs.core/src/main/java/com/bcs/core/report/export/ExportToExcelForMid.java
+++ com.bcs.core/src/main/java/com/bcs/core/report/export/ExportToExcelForMid.java
@@ -24,6 +24,10 @@ public class ExportToExcelForMid {
 	/** Logger */
 	private static Logger logger = LogManager.getLogger(ExportToExcelForMid.class);
 	
+	private SXSSFSheet[] sheetArray;
+	
+	private static int maxRowNumber = 1000000;
+	private static int maxSheetNumber = 25;
 	/**
 	 * 匯出EXCEL
 	 */
@@ -36,14 +40,14 @@ public class ExportToExcelForMid {
 //			Workbook wb = new XSSFWorkbook(); //→xls // new XSSFWorkbook()→xlsx
 			
 			SXSSFWorkbook workbook = new SXSSFWorkbook();
+			
+			//先支援到最多2500W筆資料, 但實際記憶體需要更大才能支援.
+			sheetArray = new SXSSFSheet[maxSheetNumber];
 			/**
 			 * exportMidToExcel
 			 */
-			SXSSFSheet sheetLink = workbook.createSheet(excelName);
-			// 修正關鍵字無法匯出的問題, 會重複針對Row進行讀寫, 需將windowsSize設為unlimted
-			sheetLink.setRandomAccessWindowSize(-1);			
-			this.exportMidToExcel(workbook, sheetLink, title, time, titles, data);
-			
+			this.exportMidToExcel(workbook, excelName, title, time, titles, data);
+						
 			// Save
 			FileOutputStream out = new FileOutputStream(exportPath + System.getProperty("file.separator") + fileName);
 			workbook.write(out);
@@ -58,18 +62,22 @@ public class ExportToExcelForMid {
 	/**
 	 * Export MID To Excel
 	 * @param wb
-	 * @param sheet
+	 * @param excelName
 	 * @param data
 	 * @throws Exception
 	 */
-	public void exportMidToExcel(SXSSFWorkbook workbook, SXSSFSheet sheet, String title, String time, List<List<String>> data) throws Exception{
-		this.exportMidToExcel(workbook, sheet, title, time, null, data);
+	public void exportMidToExcel(SXSSFWorkbook workbook, String excelName, String title, String time, List<List<String>> data) throws Exception{
+		this.exportMidToExcel(workbook, excelName, title, time, null, data);
 	}
 	
-	public void exportMidToExcel(SXSSFWorkbook workbook, SXSSFSheet sheet, String title, String time, List<String> titles, List<List<String>> data) throws Exception{
+	public void exportMidToExcel(SXSSFWorkbook workbook, String excelName, String title, String time, List<String> titles, List<List<String>> data) throws Exception{
 
     	log.info("Received a request to begin exportMidToExcel");
 		int index = 0;
+
+		int sheetNumber = 1;
+        int rowNumber = 0;
+        SXSSFSheet sheet = getExportMidToExcelSheet(workbook, excelName, sheetNumber);		
 		if(StringUtils.isNotBlank(title)){
 			Row row = sheet.createRow(index); // declare a row object reference
 			row.createCell(0).setCellValue(title);
@@ -104,11 +112,21 @@ public class ExportToExcelForMid {
 
 		for(int rowCount = 0; rowCount < data.size(); rowCount++){
 			List<String> cellDatas = data.get(rowCount);
+			//Re-initial when new column begin.
+			sheetNumber = 1;
+			sheet = getExportMidToExcelSheet(workbook, excelName, sheetNumber);
+			log.info("ExportMidToExcel begins to deal with New Cell Datas , cellDatas.size():{}" ,  cellDatas.size());	            	        			
 
 			for(int cell = 0; cell < cellDatas.size(); cell++){
-				Row row = sheet.getRow(cell + index);
+	            if ((cell + index) >= maxRowNumber) { // RowLimit = 1048576
+	            	sheetNumber = ((cell + index) / maxRowNumber) + 1 ; 
+                    sheet = getExportMidToExcelSheet(workbook, excelName, sheetNumber);
+                }			
+            	rowNumber = (cell + index)%maxRowNumber ;
+	            
+				Row row = sheet.getRow(rowNumber);
 				if(row == null){
-					row = sheet.createRow(cell + index);
+					row = sheet.createRow(rowNumber);
 				}
 
 				String cellData = cellDatas.get(cell);
@@ -122,4 +140,22 @@ public class ExportToExcelForMid {
 		}
     	log.info("Received a request to end exportMidToExcel");				
 	}
+	
+    private SXSSFSheet getExportMidToExcelSheet(SXSSFWorkbook workbook, String excelName, Integer sheetNumber) {
+        try {
+        	if (sheetNumber >= 1 && sheetNumber <= maxSheetNumber)  {
+        		if (sheetArray[sheetNumber-1] == null) {
+        			sheetArray[sheetNumber-1] = workbook.createSheet(excelName + sheetNumber);
+					// 修正關鍵字無法匯出的問題, 會重複針對Row進行讀寫, 需將windowsSize設為unlimted
+        			sheetArray[sheetNumber-1].setRandomAccessWindowSize(-1);
+					log.info("ExportMidToExcel get new sheet , sheetNumber:{}" ,  sheetNumber);	            	        			
+        		}
+				return sheetArray[sheetNumber-1];        		
+        	}
+        	return null;
+        } catch (Exception e) {
+            log.error(ErrorRecord.recordError(e));
+        }
+        return sheetArray[sheetNumber-1];
+    }		
 }
