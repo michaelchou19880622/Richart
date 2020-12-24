9536f5583b0b924e132f4323705c8d59983e5743
MichaelChou <mic_chou@hpicorp.com.tw>
2020-12-24 15:22:08 +0800

    1. 修正MGM活動設定判斷加入好友/綁定時，報表明細判斷被分享者完成活動是否超過上限錯誤問題。 - 改用MODIFY_TIME(被分享者點擊時間) 排序，點擊時間比較前面的要往前排，先判定為完成，後面的則判斷是否超過上限。

--- com.bcs.core/src/main/java/com/bcs/core/db/repository/ShareUserRecordRepository.java
+++ com.bcs.core/src/main/java/com/bcs/core/db/repository/ShareUserRecordRepository.java
@@ -61,7 +61,7 @@ public interface ShareUserRecordRepository extends EntityRepository<ShareUserRec
 		            	+ "and SUR.CAMPAIGN_ID = ?3 "
 		            + "order by "
 			            + "SUR.SHARE_USER_RECORD_ID, "
-			            + "SCCT.SHARED_TIME", nativeQuery = true)
+			            + "SCCT.MODIFY_TIME ASC", nativeQuery = true)
     List<Object[]> findByModifyTimeAndCampaignId_for_follow_binded(Date start, Date end, String campaignId);
     
     @Transactional(readOnly = true, timeout = 30)
